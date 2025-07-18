7ebd0901fa2494d318a1b8389f63bd357e82fce4
Jungwuk Ryu <vojougae35@gmail.com>
2025-07-18 23:52:16 +0000

    refactor: _Header 위젯 이름을 HomeHeader로 변경하고 home_header.dart 파일로 이동

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,17 +1,12 @@
-import 'package:duty_it/app/core/constants/app_colors.dart';
-import 'package:duty_it/app/modules/home/widgets/category_tag.dart';
+import 'package:duty_it/app/modules/home/controllers/home_view_controller.dart';
 import 'package:duty_it/app/modules/home/widgets/event_card.dart';
 import 'package:duty_it/app/modules/home/widgets/home_app_bar.dart';
-import 'package:duty_it/app/modules/home/widgets/home_tab_button.dart';
-import 'package:duty_it/app/modules/home/widgets/search_bar.dart';
-import 'package:duty_it/gen/assets.gen.dart';
+import 'package:duty_it/app/modules/home/widgets/home_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:persistent_header_adaptive/persistent_header_adaptive.dart';
-
-import '../controllers/home_view_controller.dart';
 
 class HomeView extends GetView<HomeViewController> {
   const HomeView({super.key});
@@ -36,7 +31,7 @@ class HomeView extends GetView<HomeViewController> {
 
           AdaptiveHeightSliverPersistentHeader(
             pinned: true,
-            child: _Header(controller: controller),
+            child: HomeHeader(controller: controller),
           ),
 
           Obx(() {
@@ -48,65 +43,6 @@ class HomeView extends GetView<HomeViewController> {
               ),
             );
           }),
-        ],
-      ),
-    );
-  }
-}
-
-class _Header extends StatelessWidget {
-  final HomeViewController controller;
-
-  const _Header({required this.controller});
-
-  @override
-  Widget build(BuildContext context) {
-    return ColoredBox(
-      color: AppColors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 20.h),
-          HomeSearchBar(controller: TextEditingController()),
-          SizedBox(height: 18.h),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Obx(() {
-                const HomeTab tab = HomeTab.event;
-                return HomeTabButton(
-                  isSelected: controller.selectedTab == tab,
-                  onTap: () => controller.selectedTab = tab,
-                  title: '행사 리스트',
-                );
-              }),
-              Obx(() {
-                const HomeTab tab = HomeTab.bookmark;
-                return HomeTabButton(
-                  isSelected: controller.selectedTab == tab,
-                  onTap: () => controller.selectedTab = tab,
-                  title: '북마크',
-                );
-              }),
-            ],
-          ),
-
-          SizedBox(height: 16.h),
-
-          Row(
-            children: [
-              const CategoryTag(name: '전체', isSelected: true),
-              SizedBox(width: 8.w),
-              CategoryTag(
-                name: '필터',
-                isSelected: false,
-                imageAsset: Assets.icons.mageFilter.path,
-              ),
-            ],
-          ),
-
-          SizedBox(height: 16.h),
         ],
       ),
     );
---
+++ lib/app/modules/home/widgets/home_header.dart
@@ -0,0 +1,68 @@
+import 'package:duty_it/app/core/constants/app_colors.dart';
+import 'package:duty_it/app/modules/home/controllers/home_view_controller.dart';
+import 'package:duty_it/app/modules/home/widgets/category_tag.dart';
+import 'package:duty_it/app/modules/home/widgets/home_tab_button.dart';
+import 'package:duty_it/app/modules/home/widgets/search_bar.dart';
+import 'package:duty_it/gen/assets.gen.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+class HomeHeader extends StatelessWidget {
+  final HomeViewController controller;
+
+  const HomeHeader({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: AppColors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20.h),
+          HomeSearchBar(controller: TextEditingController()),
+          SizedBox(height: 18.h),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Obx(() {
+                const HomeTab tab = HomeTab.event;
+                return HomeTabButton(
+                  isSelected: controller.selectedTab == tab,
+                  onTap: () => controller.selectedTab = tab,
+                  title: '행사 리스트',
+                );
+              }),
+              Obx(() {
+                const HomeTab tab = HomeTab.bookmark;
+                return HomeTabButton(
+                  isSelected: controller.selectedTab == tab,
+                  onTap: () => controller.selectedTab = tab,
+                  title: '북마크',
+                );
+              }),
+            ],
+          ),
+
+          SizedBox(height: 16.h),
+
+          Row(
+            children: [
+              const CategoryTag(name: '전체', isSelected: true),
+              SizedBox(width: 8.w),
+              CategoryTag(
+                name: '필터',
+                isSelected: false,
+                imageAsset: Assets.icons.mageFilter.path,
+              ),
+            ],
+          ),
+
+          SizedBox(height: 16.h),
+        ],
+      ),
+    );
+  }
+}
