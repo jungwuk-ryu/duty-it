1821ac2aec987075fbd3ab89d643cb5c358949c4
Jungwuk Ryu <vojougae35@gmail.com>
2025-07-19 13:57:04 +0000

    refactor: SearchFilterCategorySection 에서 SectionTitle 및 SectionSubtitle 사용

--- lib/app/modules/search_filter/widgets/search_filter_category_section.dart
+++ lib/app/modules/search_filter/widgets/search_filter_category_section.dart
@@ -1,5 +1,6 @@
-import 'package:duty_it/app/core/constants/app_colors.dart';
 import 'package:duty_it/app/modules/search_filter/controllers/search_filter_view_controller.dart';
+import 'package:duty_it/app/modules/search_filter/widgets/search_filter_section_subtitle.dart';
+import 'package:duty_it/app/modules/search_filter/widgets/search_filter_section_title.dart';
 import 'package:duty_it/app/widgets/category_tag.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,29 +18,13 @@ class SearchFilterCategorySection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              '카테고리',
-              style: TextStyle(
-                color: AppColors.black,
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                height: 1.60,
-              ),
-            ),
+            SearchFilterSectionTitle("카테고리"),
             SizedBox(width: 8.w),
-            Text(
-              '중복 선택 가능',
-              style: TextStyle(
-                color: AppColors.g05,
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                height: 1.60,
-              ),
-            ),
+            SearchFilterSectionSubtitle('중복 선택 가능'),
           ],
         ),
-        SizedBox(height: 8.h),
         Wrap(
           spacing: 8.w,
           runSpacing: 8.h,
