e4958716945def39b435ce00617b6428f74d667b
Jungwuk Ryu <vojougae35@gmail.com>
2025-07-19 00:49:03 +0000

    refactor: CustomDivider 추가

--- lib/app/widgets/custom_divider.dart
+++ lib/app/widgets/custom_divider.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:duty_it/app/core/constants/app_colors.dart';
 
-class AccountViewDivider extends StatelessWidget {
-  const AccountViewDivider({super.key});
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 8.h,
-      width: double.infinity,
-      child: ColoredBox(color: AppColors.g01),
+    return ColoredBox(
+      color: AppColors.g01,
+      child: SizedBox(height: 8.h, width: double.infinity),
     );
   }
 }
