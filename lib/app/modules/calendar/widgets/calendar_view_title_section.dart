8cfca2d85fa1041b19838942d91c215a0dff069f
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-01 13:27:29 +0000

    feat: CalendarViewTitleSection에 년도와 아이콘 추가

--- lib/app/modules/calendar/widgets/calendar_view_title_section.dart
+++ lib/app/modules/calendar/widgets/calendar_view_title_section.dart
@@ -1,5 +1,7 @@
 import 'package:duty_it/app/core/constants/app_colors.dart';
+import 'package:duty_it/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CalendarViewTitleSection extends StatelessWidget {
   final DateTime dt;
@@ -8,14 +10,46 @@ class CalendarViewTitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "${dt.month}월",
-      style: TextStyle(
-        color: AppColors.black,
-        fontSize: 32,
-        fontWeight: FontWeight.w700,
-        height: 1.60,
-      ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          "${dt.month}월",
+          style: TextStyle(
+            color: AppColors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+            height: 1.60,
+          ),
+        ),
+        SizedBox(width: 7.w),
+        Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '${dt.year}',
+                  style: TextStyle(
+                    color: const Color(0xFF949494),
+                    fontSize: 15,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w700,
+                    height: 1.60,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Image.asset(
+                  Assets.icons.iconGoG05.path,
+                  width: 16.r,
+                  height: 16.r,
+                ),
+              ],
+            ),
+            SizedBox(height: 7.h),
+          ],
+        ),
+      ],
     );
   }
 }
