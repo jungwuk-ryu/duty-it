f474c876505a419a87f31fae2396dc03cbae91d6
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-09 05:11:23 +0000

    feat: 푸시 알림 비활성화시 하위 알림 설정 비활성화

---
+++ lib/app/modules/settings/widgets/toggle_setting_item.dart
@@ -0,0 +1,44 @@
+import 'package:duty_it/app/core/constants/app_colors.dart';
+import 'package:duty_it/app/widgets/custom_toggle_button.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+class ToggleSettingItem extends StatelessWidget {
+  final String text;
+  final bool checked;
+  final VoidCallback onToggleTap;
+  final bool enabled;
+
+  const ToggleSettingItem({
+    super.key,
+    required this.text,
+    required this.checked,
+    required this.onToggleTap,
+    required this.enabled,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            color: enabled ? AppColors.black : AppColors.cal1,
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+            height: 1.60,
+          ),
+        ),
+        Expanded(child: SizedBox()),
+        CustomToggleButton(
+            checked: checked,
+            enabled: enabled,
+            onTap: () {
+              if (enabled) onToggleTap();
+            },
+          ),
+      ],
+    );
+  }
+}
