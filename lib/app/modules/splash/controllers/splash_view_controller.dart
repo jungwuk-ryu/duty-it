2d453e3d05293afa62eb1576dcd7a365f99b9a3c
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-17 07:07:32 +0000

    refactor: SplashViewController에서 로그인 상태 확인시 AuthService 이용하도록 변경

--- lib/app/modules/splash/controllers/splash_view_controller.dart
+++ lib/app/modules/splash/controllers/splash_view_controller.dart
@@ -1,5 +1,5 @@
 import 'package:duty_it/app/routes/app_pages.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:duty_it/app/services/auth/auth_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
@@ -11,17 +11,19 @@ class SplashViewController extends GetxController {
     await Future.delayed(Duration(seconds: 2));
 
     try {
-      User? user = FirebaseAuth.instance.currentUser;
-      if (user == null) {
-        Get.offAllNamed(Routes.LOGIN);
-      } else {
+      var authService = Get.find<AuthService>();
+
+      if (authService.isLoggined()) {
         Get.offAllNamed(Routes.MAIN);
+      } else {
+        Get.offAllNamed(Routes.LOGIN);
       }
     } catch (e) {
       // TODO : Crashlytics 로깅
       if (kDebugMode) {
         print(e);
       }
+
       Get.offAllNamed(Routes.LOGIN);
     }
   }
