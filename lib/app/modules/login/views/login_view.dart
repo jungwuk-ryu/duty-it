5d85dd85e43fc31b194882bb04fc50551f540ddf
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-17 06:58:24 +0000

    feat: LoginView를 Scaffold로 감쌈

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -11,42 +11,44 @@ class LoginView extends GetView<LoginViewController> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Hero(tag: SplashView.heroKey, child: Text('LOGO')),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 88.h,
-          child: GestureDetector(
-            onTap: controller.onLoginButtonTap,
-            child: Image.asset(
-              Assets.icons.kakaoLoginLargeWide.path,
-              width: 328.w,
-              height: 50.h,
-            ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Hero(tag: SplashView.heroKey, child: Text('LOGO')),
           ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: -12.h,
-          child: Obx(
-            () => Visibility(
-              visible: controller.isLogining,
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 88.h,
+            child: GestureDetector(
+              onTap: controller.onLoginButtonTap,
               child: Image.asset(
-                Assets.icons.loadingIndicator.path,
-                width: 58.w,
-                height: 7.h,
+                Assets.icons.kakaoLoginLargeWide.path,
+                width: 328.w,
+                height: 50.h,
               ),
             ),
           ),
-        ),
-      ],
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: -12.h,
+            child: Obx(
+              () => Visibility(
+                visible: controller.isLogining,
+                child: Image.asset(
+                  Assets.icons.loadingIndicator.path,
+                  width: 58.w,
+                  height: 7.h,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
