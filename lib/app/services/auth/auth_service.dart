2bd59d10a0ce044fec6e852ff7f589e554fe2143
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-30 02:44:31 +0000

    feat: AuthService가 AppUser를 관리함

--- lib/app/services/auth/auth_service.dart
+++ lib/app/services/auth/auth_service.dart
@@ -1,13 +1,33 @@
+import 'dart:convert';
+
+import 'package:duty_it/app/models/app_user.dart';
 import 'package:duty_it/app/services/auth/models/social_login_result.dart';
 import 'package:duty_it/app/services/auth/strategies/kakao_login_strategy.dart';
 import 'package:duty_it/app/services/auth/strategies/social_login_strategy.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 enum SocialProvider { kakao }
 
 class AuthService extends GetxService {
+  static const String storageBoxName = "authService";
+  static const String _appUserKey = 'app_user';
+
   final Map<SocialProvider, SocialLoginStrategy> _strategies = {};
+  final Rxn<AppUser> _appUser = Rxn();
+  late final GetStorage _box;
+
+  AppUser? get appUser => _appUser.value;
+  set appUser(AppUser? user) {
+    _appUser.value = user;
+
+    if (user != null) {
+      _box.write(_appUserKey, user.toJson());
+    } else {
+      _box.remove(_appUserKey);
+    }
+  }
 
   SocialLoginStrategy? _currentStrategy;
 
@@ -15,6 +35,18 @@ class AuthService extends GetxService {
   void onInit() {
     super.onInit();
     _initStrategies();
+
+    _box = GetStorage(storageBoxName);
+    _loadCachedAppUser();
+  }
+
+  void _loadCachedAppUser() {
+    String? jsonStr = _box.read(_appUserKey);
+    if (jsonStr == null) return;
+
+    try {
+      _appUser.value = AppUser.fromJson(json.decode(jsonStr));
+    } catch (_) {}
   }
 
   void _initStrategies() {
@@ -37,6 +69,7 @@ class AuthService extends GetxService {
 
   Future<void> logout() async {
     await _currentStrategy?.logout();
+    appUser = null;
     _currentStrategy = null;
   }
 
