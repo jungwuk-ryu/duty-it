af2a40a45acf0ef82dab26aefb752b52090f8d0d
Jungwuk Ryu <vojougae35@gmail.com>
2025-08-30 05:47:12 +0000

    feat: 로그아웃/회원 탈퇴 다이얼로그 추가

--- lib/app/modules/account/controllers/account_view_controller.dart
+++ lib/app/modules/account/controllers/account_view_controller.dart
@@ -1,10 +1,11 @@
 import 'package:duty_it/app/api_client.dart';
+import 'package:duty_it/app/modules/account/widgets/account_bottom_modal.dart';
+import 'package:duty_it/app/modules/account/widgets/account_dialog.dart';
 import 'package:duty_it/app/routes/app_pages.dart';
 import 'package:duty_it/app/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:duty_it/app/modules/account/widgets/account_bottom_modal.dart';
 
 class AccountViewController extends GetxController {
   AuthService get _authService => Get.find<AuthService>();
@@ -36,12 +37,15 @@ class AccountViewController extends GetxController {
   }
 
   Future<bool> setUserName(String newName) async {
-    var result = await Get.find<ApiClient>().updateCurrentUserNickname(newName.trim());
+    var result = await Get.find<ApiClient>().updateCurrentUserNickname(
+      newName.trim(),
+    );
     return result is RequestSuccess;
   }
 
   Future<bool> isNicknameAvailable(String nickname) async {
-    return await Get.find<ApiClient>().isNicknameAvailable(nickname) is RequestSuccess;
+    return await Get.find<ApiClient>().isNicknameAvailable(nickname)
+        is RequestSuccess;
   }
 
   String getAccountId() {
@@ -49,13 +53,32 @@ class AccountViewController extends GetxController {
   }
 
   Future<void> logout() async {
-    await _authService.logout();
-    Get.offAndToNamed(Routes.LOGIN);
+    Get.dialog(
+      AccountDialog(
+        title: '로그아웃',
+        content: '로그아웃 하시겠습니까?',
+        actionText: '로그아웃',
+        action: () async {
+          Get.back();
+          await _authService.logout();
+          Get.offAndToNamed(Routes.LOGIN);
+        },
+      ),
+    );
   }
 
   Future<void> withdraw() async {
-    if (await _authService.withdraw()) {
-      Get.offAndToNamed(Routes.LOGIN);
-    }
+    Get.dialog(
+      AccountDialog(
+        title: '회원 탈퇴',
+        content: '탈퇴 하시겠습니까?\n이 작업은 복구할 수 없습니다.',
+        actionText: '탈퇴',
+        action: () async {
+          if (await _authService.withdraw()) {
+            Get.offAndToNamed(Routes.LOGIN);
+          }
+        },
+      ),
+    );
   }
 }
