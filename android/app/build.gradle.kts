b86a7dd9e8becfaadb03ee6bb475cbd30e7fb1de
vojougae35 <vojougae35@gmail.com>
2025-09-06 09:47:07 +0000

    chore(android): minSdkVersion을 gradle.properties에서 지정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dutyit.app"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
