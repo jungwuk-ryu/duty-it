64e6692182d24c4b89d76f65f362cdee1ca8aac1
vojougae35 <vojougae35@gmail.com>
2025-09-06 10:47:34 +0000

    feat: 구글 로그인 client ID 지정

--- lib/app/services/auth/strategies/google_login_strategy.dart
+++ lib/app/services/auth/strategies/google_login_strategy.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:duty_it/app/services/auth/models/social_login_result.dart';
 import 'package:duty_it/app/services/auth/strategies/social_login_strategy.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,13 +10,12 @@ class GoogleLoginStrategy extends SocialLoginStrategy {
   @override
   Future<SocialLoginResult> login() async {
     try {
-      GoogleSignIn googleSignIn = GoogleSignIn.instance;
-
       if (kIsWeb) {
         final provider = GoogleAuthProvider();
         await FirebaseAuth.instance.signInWithPopup(provider);
         return SocialLoginSuccess();
       }
+      
 
       final GoogleSignInAccount googleUser = await googleSignIn.authenticate();
       final GoogleSignInAuthentication googleAuth = googleUser.authentication;
@@ -27,6 +28,7 @@ class GoogleLoginStrategy extends SocialLoginStrategy {
       await FirebaseAuth.instance.signInWithCredential(credential);
       return SocialLoginSuccess();
     } catch (e, st) {
+      if (kDebugMode) rethrow;
       return SocialLoginFail(reason: kDebugMode ? "$e\n$st" : '로그인 실패');
     }
   }
