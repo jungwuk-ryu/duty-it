f5bd45288563f08ba7b956ca5fd4838124d46758
Jungwuk Ryu <vojougae35@gmail.com>
2025-09-06 07:27:35 +0000

    feat: 마지막 앱 사용 버전 기록

--- lib/app/modules/splash/controllers/splash_view_controller.dart
+++ lib/app/modules/splash/controllers/splash_view_controller.dart
@@ -5,18 +5,26 @@ import 'package:duty_it/app/services/search_filter/search_filter_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashViewController extends GetxController {
   @override
   void onReady() async {
     super.onReady();
+    final String appInfoBoxName = "appInfo";
 
-    await Future.wait([
+    var results = await Future.wait([
+      PackageInfo.fromPlatform(),
       GetStorage.init(AppSettingsService.storageBoxName),
       GetStorage.init(AuthService.storageBoxName),
       GetStorage.init(SearchFilterService.storageBoxName),
+      GetStorage.init(appInfoBoxName),
       Future.delayed(Duration(seconds: 1)),
     ]);
+    PackageInfo packageInfo = results[0];
+    GetStorage appInfoBox = GetStorage(appInfoBoxName);
+    //String? oldVersion = appInfoBox.read('old_version');
+    appInfoBox.write('old_version', packageInfo.version);
 
     try {
       var authService = Get.find<AuthService>();
