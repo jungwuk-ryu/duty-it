8b81c429cae576cabaf1d2b71c0ad19f35c55363
Jungwuk Ryu <vojougae35@gmail.com>
2025-09-06 09:31:49 +0000

    chore(android): 빌드 이슈 해결

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+}
+
 allprojects {
     repositories {
         google()
