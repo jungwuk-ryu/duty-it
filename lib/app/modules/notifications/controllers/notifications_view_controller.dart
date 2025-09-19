035729b480a832d2ceeb2c980e336f1cb1313826
Jungwuk Ryu <vojougae35@gmail.com>
2025-09-19 14:16:00 +0000

    refactor: NotificationRepository 리팩토링
    resolve #55

--- lib/app/modules/notifications/controllers/notifications_view_controller.dart
+++ lib/app/modules/notifications/controllers/notifications_view_controller.dart
@@ -1,36 +1,20 @@
 import 'package:duty_it/app/modules/notifications/models/fcm_notification.dart';
 import 'package:duty_it/app/modules/notifications/repositories/notification_repository.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
-class NotificationsViewController extends GetxController
-    with WidgetsBindingObserver {
+class NotificationsViewController extends GetxController {
   NotificationRepository get repo => Get.find<NotificationRepository>();
   RxList notificationList = RxList();
 
   @override
   void onInit() {
     super.onInit();
-    WidgetsBinding.instance.addObserver(this);
-    _initList();
-  }
-
-  Future<void> _initList() async {
-    await repo.loadFromDisk();
     loadNotificationList();
-  }
 
-  @override
-  void onClose() {
-    super.onClose();
-    WidgetsBinding.instance.removeObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      repo.loadFromDisk();
-    }
+    repo.addNotification(RemoteMessage(sentTime: DateTime.now(), notification: RemoteNotification(title: 'test', body: 'test')));
+    repo.addNotification(RemoteMessage(sentTime: DateTime.now(), notification: RemoteNotification(title: 'test2', body: 'test2')));
+    repo.addNotification(RemoteMessage(sentTime: DateTime.now(), notification: RemoteNotification(title: 'test3', body: 'test3')));
   }
 
   Future<void> loadNotificationList({bool markAsRead = true}) async {
