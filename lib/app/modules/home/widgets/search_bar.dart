6df0885c1cd2ea951547697f7d28e5b7877c89d0
Jungwuk Ryu <vojougae35@gmail.com>
2025-09-20 11:50:13 +0000

    feat: 홈 검색어 초기화 버튼 추가
    resolve #65

--- lib/app/modules/home/widgets/search_bar.dart
+++ lib/app/modules/home/widgets/search_bar.dart
@@ -1,13 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:duty_it/app/core/constants/app_colors.dart';
 import 'package:duty_it/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeSearchBar extends StatelessWidget {
+class HomeSearchBar extends StatefulWidget {
   final TextEditingController controller;
 
   const HomeSearchBar({super.key, required this.controller});
 
+  @override
+  State<HomeSearchBar> createState() => _HomeSearchBarState();
+}
+
+class _HomeSearchBarState extends State<HomeSearchBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,8 +36,9 @@ class HomeSearchBar extends StatelessWidget {
             SizedBox(width: 8),
             Expanded(
               child: TextField(
-                controller: controller,
+                controller: widget.controller,
                 onTapUpOutside: (_) => FocusScope.of(context).unfocus(),
+                onChanged: (_) => setState(() {}),
                 decoration: InputDecoration(
                   isCollapsed: true,
                   hintText: '찾으시는 행사가 있나요?',
@@ -45,6 +51,24 @@ class HomeSearchBar extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(width: 12),
+            if (widget.controller.text.isNotEmpty)
+              Padding(
+                padding: EdgeInsetsGeometry.only(right: 12),
+                child: GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTap: () {
+                    setState(() {
+                      widget.controller.clear();
+                    });
+                  },
+                  child: Image.asset(
+                    Assets.icons.textdelete.path,
+                    width: 16.r,
+                    height: 16.r,
+                  ),
+                ),
+              ),
           ],
         ),
       ),
