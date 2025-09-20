fcd7d57ebc0fe931e12e39ce04cc50b5c53a4338
Jungwuk Ryu <vojougae35@gmail.com>
2025-09-20 08:01:13 +0000

    feat: 캘린더 월 선택 모달 디자인 요구사항 반영
    resolves #25

--- lib/app/modules/calendar/widgets/date_selection_bottom_modal.dart
+++ lib/app/modules/calendar/widgets/date_selection_bottom_modal.dart
@@ -1,11 +1,13 @@
+import 'dart:math';
+
 import 'package:duty_it/app/core/constants/app_colors.dart';
 import 'package:duty_it/app/modules/calendar/controllers/date_selection_modal_controller.dart';
 import 'package:duty_it/app/widgets/app_normal_button.dart';
 import 'package:duty_it/gen/assets.gen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 class DateSelectionBottomModal extends StatelessWidget {
   DateSelectionModalController get controller =>
@@ -15,7 +17,8 @@ class DateSelectionBottomModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double itemExtent = 51.h;
+    final double itemExtent = 50;
+    final double startYear = 2000;
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 13.w),
@@ -53,56 +56,118 @@ class DateSelectionBottomModal extends StatelessWidget {
             ],
           ),
           SizedBox(height: 20.h),
-          SizedBox(
-            height: itemExtent * 2,
-            child: CupertinoPicker.builder(
-              // selectionOverlay: Container(
-              //   height: itemExtent,
-              //   width: double.infinity,
-              //   decoration: BoxDecoration(
-              //     color: AppColors.cal3.withValues(alpha: 152),
-              //     borderRadius: BorderRadius.circular(8),
-              //   ),
-              // ),
-              itemExtent: itemExtent,
-              diameterRatio: 10,
-              onSelectedItemChanged: (i) {
-                controller.selectedItemIndex = i;
-              },
-              itemBuilder: (_, i) {
-                DateTime ndt = controller.getDateTimeByIndex(i);
 
-                return Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(child: SizedBox()),
-                      Text(
-                        '${ndt.year}년',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          height: 1.60,
+          SizedBox(
+            height: itemExtent * 3,
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: itemExtent * 3,
+                        child: ScrollSnapList(
+                          scrollDirection: Axis.vertical,
+                          itemCount: 100,
+                          initialIndex: max(
+                            controller.baseDate.year - startYear,
+                            0,
+                          ),
+                          itemSize: itemExtent,
+                          onItemFocus: (i) {
+                            controller.selectedYear = startYear + i as int;
+                          },
+                          itemBuilder: (context, i) {
+                            final year = startYear + i as int;
+
+                            return SizedBox(
+                              height: itemExtent,
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Obx(() {
+                                  bool selected =
+                                      controller.selectedYear == year;
+                                  return AnimatedDefaultTextStyle(
+                                    style: TextStyle(
+                                      color: selected
+                                          ? AppColors.black
+                                          : AppColors.g05,
+                                      fontSize: selected ? 16 : 14,
+                                      fontWeight: FontWeight.w600,
+                                      height: 1.60,
+                                    ),
+                                    duration: Duration(milliseconds: 100),
+                                    child: Text(
+                                      '$year년',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                }),
+                              ),
+                            );
+                          },
                         ),
                       ),
-                      SizedBox(width: 64.w),
-                      Text(
-                        '${ndt.month}월',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          height: 1.60,
+                    ),
+                    const SizedBox(width: 64),
+                    Expanded(
+                      child: SizedBox(
+                        height: itemExtent * 3,
+                        child: ScrollSnapList(
+                          scrollDirection: Axis.vertical,
+                          itemCount: 12,
+                          initialIndex: controller.baseDate.month - 1,
+                          itemSize: itemExtent,
+                          onItemFocus: (index) {
+                            controller.selectedMonth = index + 1;
+                          },
+                          itemBuilder: (context, i) {
+                            return SizedBox(
+                              height: itemExtent,
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Obx(() {
+                                  bool selected =
+                                      controller.selectedMonth == i + 1;
+                                  return AnimatedDefaultTextStyle(
+                                    style: TextStyle(
+                                      color: selected
+                                          ? AppColors.black
+                                          : AppColors.g05,
+                                      fontSize: selected ? 16 : 14,
+                                      fontWeight: FontWeight.w600,
+                                      height: 1.60,
+                                    ),
+                                    duration: Duration(milliseconds: 100),
+                                    child: Text(
+                                      '${i + 1}월',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                }),
+                              ),
+                            );
+                          },
                         ),
                       ),
-                      Expanded(child: SizedBox()),
-                    ],
+                    ),
+                  ],
+                ),
+
+                Align(
+                  alignment: Alignment.center,
+                  child: IgnorePointer(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.g07.withAlpha(37),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      width: double.infinity,
+                      height: itemExtent,
+                    ),
                   ),
-                );
-              },
+                ),
+              ],
             ),
           ),
           SizedBox(height: 30.h),
