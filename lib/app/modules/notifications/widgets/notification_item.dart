47d44bb21f940cdf682cd05f88f8c7f64b791f5b
Jungwuk Ryu <vojougae35@gmail.com>
2025-11-02 02:01:05 +0000

    feat: 알람 클릭시 행사 주소로 이동

--- lib/app/modules/notifications/widgets/notification_item.dart
+++ lib/app/modules/notifications/widgets/notification_item.dart
@@ -5,6 +5,7 @@ import 'package:duty_it/app/core/models/event.dart';
 import 'package:duty_it/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class NotificationItem extends StatelessWidget {
   final AppNotification noti;
@@ -38,74 +39,84 @@ class NotificationItem extends StatelessWidget {
             Get.find<NotificationsViewController>();
         await con.deleteNotification(noti.id);
       },
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        decoration: BoxDecoration(
-          color: !noti.isRead ? AppColors.cal2 : AppColors.white,
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: 40,
-              height: 40,
-              padding: EdgeInsets.only(
-                top: 11,
-                right: 10,
-                bottom: 11,
-                left: 12,
-              ),
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: OvalBorder(
-                  side: BorderSide(
-                    width: 1,
-                    strokeAlign: BorderSide.strokeAlignOutside,
-                    color: const Color(0xFFD0D0D0),
+      child: InkWell(
+        onTap: () async {
+          if (noti.event == null) return;
+          Event event = noti.event!;
+          if (await canLaunchUrlString(event.uri)) launchUrlString(event.uri);
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: !noti.isRead ? AppColors.cal2 : AppColors.white,
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                padding: EdgeInsets.only(
+                  top: 11,
+                  right: 10,
+                  bottom: 11,
+                  left: 12,
+                ),
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: OvalBorder(
+                    side: BorderSide(
+                      width: 1,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                      color: const Color(0xFFD0D0D0),
+                    ),
+                  ),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    Assets.icons.logo.path,
+                    fit: BoxFit.contain,
                   ),
                 ),
               ),
-              child: Center(
-                child: Image.asset(Assets.icons.logo.path, fit: BoxFit.contain),
+              SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _getTitle(noti),
+                      style: TextStyle(
+                        color: const Color(0xFF333333),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        height: 1.60,
+                      ),
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      _getContent(noti),
+                      style: TextStyle(
+                        color: const Color(0xFF6F6F6F),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                        height: 1.60,
+                      ),
+                    ),
+                    Text(
+                      _formatDateTime(noti.createdAt),
+                      style: TextStyle(
+                        color: const Color(0xFF949494),
+                        fontSize: 8,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                        height: 1.60,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(width: 15),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _getTitle(noti),
-                    style: TextStyle(
-                      color: const Color(0xFF333333),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      height: 1.60,
-                    ),
-                  ),
-                  SizedBox(height: 2),
-                  Text(
-                    _getContent(noti),
-                    style: TextStyle(
-                      color: const Color(0xFF6F6F6F),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                      height: 1.60,
-                    ),
-                  ),
-                  Text(
-                    _formatDateTime(noti.createdAt),
-                    style: TextStyle(
-                      color: const Color(0xFF949494),
-                      fontSize: 8,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w500,
-                      height: 1.60,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
