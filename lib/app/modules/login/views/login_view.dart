2dd0071d347d863606bed27b9594ad0ae414b59a
Jungwuk Ryu <vojougae35@gmail.com>
2025-11-02 09:57:09 +0000

    feat: 로그인 페이지 앱 바 추가

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:duty_it/app/modules/login/widgets/login_button.dart';
 import 'package:duty_it/app/modules/splash/views/splash_view.dart';
 import 'package:duty_it/app/services/auth/auth_service.dart';
+import 'package:duty_it/app/widgets/simple_app_bar.dart';
 import 'package:duty_it/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,50 +15,58 @@ class LoginView extends GetView<LoginViewController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 16),
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            children: [
-              Spacer(),
-              Hero(
-                tag: SplashView.heroKey,
-                child: Image.asset(
-                  Assets.icons.logo.path,
-                  width: 84,
-                  height: 80,
-                ),
-              ),
-              Spacer(),
-              Column(
-                spacing: 16,
-                children: [
-                  /*LoginButton(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SimpleAppBar(title: '로그인'),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 50, left: 16, right: 16),
+                child: Column(
+                  children: [
+                    Spacer(),
+                    Hero(
+                      tag: SplashView.heroKey,
+                      child: Image.asset(
+                        Assets.icons.logo.path,
+                        width: 84,
+                        height: 80,
+                      ),
+                    ),
+                    Spacer(),
+                    Column(
+                      spacing: 16,
+                      children: [
+                        /*LoginButton(
                     iconPath: Assets.icons.kakao.path,
                     providerName: "Kakao",
                     buttonColor: Color(0xFFFEE500),
                     onTap: () async =>
                         controller.onLoginButtonTap(SocialProvider.kakao),
                   ),*/
-                  LoginButton(
-                    iconPath: Assets.icons.appleWhite.path,
-                    buttonColor: Color(0xFF000000),
-                    providerName: "Apple",
-                    onTap: () async =>
-                        controller.onLoginButtonTap(SocialProvider.apple),
-                  ),
-                  LoginButton(
-                    iconPath: Assets.icons.google.path,
-                    buttonColor: Color(0xFFF2F2F2),
-                    providerName: "Google",
-                    onTap: () async =>
-                        controller.onLoginButtonTap(SocialProvider.google),
-                  ),
-                ],
+                        LoginButton(
+                          iconPath: Assets.icons.appleWhite.path,
+                          buttonColor: Color(0xFF000000),
+                          providerName: "Apple",
+                          onTap: () async =>
+                              controller.onLoginButtonTap(SocialProvider.apple),
+                        ),
+                        LoginButton(
+                          iconPath: Assets.icons.google.path,
+                          buttonColor: Color(0xFFF2F2F2),
+                          providerName: "Google",
+                          onTap: () async => controller.onLoginButtonTap(
+                            SocialProvider.google,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
