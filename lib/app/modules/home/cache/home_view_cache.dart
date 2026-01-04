2d304525f6ea4021ddcff5c90766a3c9d3480ee7
Jungwuk Ryu <vojougae35@gmail.com>
2026-01-04 06:05:54 +0000

    refactor: HomeViewCache

--- lib/app/modules/home/cache/home_view_cache.dart
+++ lib/app/modules/home/cache/home_view_cache.dart
@@ -3,21 +3,22 @@ import 'package:get_storage/get_storage.dart';
 
 class HomeViewCache {
   static final String boxName = "homeContainer";
-  static final String eventListKey = "event_list";
-  static final String eventsUrlKey = "request_url";
-  static final String eventsLastUpdateCacheKey = "last_update";
+   static const int _cacheValidityInDays = 7;
+  static const String _eventListKey = "event_list";
+  static const String _eventsUrlKey = "request_url";
+  static const String _eventsLastUpdateCacheKey = "last_update";
 
   final GetStorage _box = GetStorage(boxName);
 
   EventsResponse? getEvents() {
-    final Map? listCache = _box.read(eventListKey);
-    final int? lastUpdateMills = _box.read(eventsLastUpdateCacheKey);
+    final Map? listCache = _box.read(_eventListKey);
+    final int? lastUpdateMills = _box.read(_eventsLastUpdateCacheKey);
     final DateTime? lastUpdate = lastUpdateMills != null
         ? DateTime.fromMillisecondsSinceEpoch(lastUpdateMills)
         : null;
     final bool isValid =
         lastUpdate != null &&
-        DateTime.now().difference(lastUpdate).inDays < 7 &&
+        DateTime.now().difference(lastUpdate).inDays < _cacheValidityInDays &&
         listCache != null;
 
     if (!isValid) return null;
@@ -27,16 +28,16 @@ class HomeViewCache {
 
   Future<void> saveEvents(EventsResponse rep) async {
     await Future.wait([
-      _box.write(eventListKey, rep.toJson()),
-      if (rep.reqUrl != null) _box.write(eventsUrlKey, rep.reqUrl),
+      _box.write(_eventListKey, rep.toJson()),
+      if (rep.reqUrl != null) _box.write(_eventsUrlKey, rep.reqUrl),
       _box.write(
-        eventsLastUpdateCacheKey,
+        _eventsLastUpdateCacheKey,
         DateTime.now().millisecondsSinceEpoch,
       ),
     ]);
   }
 
   String? getEventsUrl() {
-    return _box.read(eventsUrlKey);
+    return _box.read(_eventsUrlKey);
   }
 }
