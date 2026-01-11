cab11b90f96e8d410438d110c22ffe139a87c709
Jungwuk Ryu <vojougae35@gmail.com>
2026-01-11 06:30:17 +0000

    feat: AppNormalButton 디바운스

--- lib/app/widgets/app_normal_button.dart
+++ lib/app/widgets/app_normal_button.dart
@@ -1,10 +1,13 @@
 import 'package:duty_it/app/core/constants/app_colors.dart';
+import 'package:duty_it/app/core/utils/app_utils.dart';
 import 'package:duty_it/app/widgets/tap_scale.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
-class AppNormalButton extends StatelessWidget {
+class AppNormalButton extends StatefulWidget {
   final String text;
-  final VoidCallback onTap;
+  final Function() onTap;
   final Color? color;
 
   const AppNormalButton({
@@ -15,19 +18,26 @@ class AppNormalButton extends StatelessWidget {
   });
 
   @override
+  State<AppNormalButton> createState() => _AppNormalButtonState();
+}
+
+class _AppNormalButtonState extends State<AppNormalButton> {
+  bool isCallbackRunning = false;
+
+  @override
   Widget build(BuildContext context) {
     return TapScale(
       child: GestureDetector(
-        onTap: onTap,
+        onTap: tap,
         child: Container(
           width: double.infinity,
           padding: EdgeInsets.symmetric(vertical: 14),
           decoration: BoxDecoration(
-            color: color ?? AppColors.main,
+            color: widget.color ?? AppColors.main,
             borderRadius: BorderRadius.circular(8),
           ),
           child: Text(
-            text,
+            widget.text,
             textAlign: TextAlign.center,
             style: TextStyle(
               color: AppColors.white,
@@ -39,5 +49,27 @@ class AppNormalButton extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> tap() async {
+    if (isCallbackRunning) return;
+    isCallbackRunning = true;
+
+    try {
+      await widget.onTap();
+    } catch (e, st) {
+      if (kDebugMode) {
+        print("앱 버튼 클릭 callback 실행 중 오류 발생: $e\n$st");
+      }
+
+      AppUtils.showSnackBar('오류가 발생했어요. 다시 시도해주세요\n');
+      FirebaseCrashlytics.instance.recordError(
+        e,
+        st,
+        reason: "앱 버튼 클릭 callback 실행 중 오류 발생",
+      );
+    } finally {
+      isCallbackRunning = false;
+    }
   }
 }
