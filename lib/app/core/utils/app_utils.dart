b251c3c92d7800bd3d714b2cffe58e1ee073b603
Jungwuk Ryu <vojougae35@gmail.com>
2026-01-11 09:20:27 +0000

    feat: DateTime 시작일, 종료일 구하는 util 추가

--- lib/app/core/utils/app_utils.dart
+++ lib/app/core/utils/app_utils.dart
@@ -1,11 +1,67 @@
+import 'package:duty_it/app/core/constants/app_colors.dart';
 import 'package:duty_it/app/routes/app_pages.dart';
 import 'package:flutter/widgets.dart';
-
 import 'package:get/get.dart';
-import 'package:duty_it/app/core/constants/app_colors.dart';
 
 class AppUtils {
   AppUtils._();
+
+  static DateTime dateTime2Date(DateTime dt) {
+    return dt.copyWith(
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+      microsecond: 0,
+    );
+  }
+
+  static DateTime endOfMonth(DateTime date) {
+    return DateTime(date.year, date.month + 1, 0);
+  }
+
+  /// 2025년 00월 00일(일) 형태
+  static String formatDateTime(DateTime dt) {
+    final mm = dt.month.toString().padLeft(2, '0');
+    final dd = dt.day.toString().padLeft(2, '0');
+    return '${dt.year}년 $mm월 $dd일(${weekDay2Text(dt.weekday)})';
+  }
+
+  static bool hasTime(DateTime dt) {
+    return dt.microsecond != 0 ||
+        dt.millisecond != 0 ||
+        dt.second != 0 ||
+        dt.minute != 0 ||
+        dt.hour != 0;
+  }
+
+  static bool isDateWithin(DateTime target, DateTime start, DateTime end) {
+    if (hasTime(target)) target = dateTime2Date(target);
+    if (hasTime(start)) start = dateTime2Date(start);
+    if (hasTime(end)) end = dateTime2Date(end);
+
+    return (start.isBefore(target) || AppUtils.isSameDay(start, target)) &&
+        !end.isBefore(target);
+  }
+
+  static bool isSameDay(DateTime dt1, DateTime dt2) {
+    return dt1.day == dt2.day && dt1.month == dt2.month && dt1.year == dt2.year;
+  }
+
+  static Future<void> resetApp() async {
+    await Get.deleteAll(force: true);
+    Get.offAllNamed(Routes.SPLASH);
+  }
+
+  static Uri setDuitUtmSource(Uri uri) {
+    return uri.replace(
+      queryParameters: {...uri.queryParameters, 'utm_source': 'www.dutyit.net'},
+    );
+  }
+
+  static String setDuitUtmSourceString(String uri) {
+    return setDuitUtmSource(Uri.parse(uri)).toString();
+  }
 
   static void showSnackBar(String content, {Widget? mainButton}) {
     Get.showSnackbar(
@@ -33,64 +89,12 @@ class AppUtils {
     );
   }
 
+  static DateTime startOfMonth(DateTime date) {
+    return DateTime(date.year, date.month, 1);
+  }
+
   static String weekDay2Text(int weekDay) {
     final List<String> texts = ['월', '화', '수', '목', '금', '토', '일'];
     return texts[weekDay - 1];
-  }
-
-  static bool isSameDay(DateTime dt1, DateTime dt2) {
-    return dt1.day == dt2.day && dt1.month == dt2.month && dt1.year == dt2.year;
-  }
-
-  static DateTime dateTime2Date(DateTime dt) {
-    return dt.copyWith(
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-      microsecond: 0,
-    );
-  }
-
-  static bool isDateWithin(DateTime target, DateTime start, DateTime end) {
-    if (hasTime(target)) target = dateTime2Date(target);
-    if (hasTime(start)) start = dateTime2Date(start);
-    if (hasTime(end)) end = dateTime2Date(end);
-
-    return (start.isBefore(target) || AppUtils.isSameDay(start, target)) &&
-        !end.isBefore(target);
-  }
-
-  static bool hasTime(DateTime dt) {
-    return dt.microsecond != 0 ||
-        dt.millisecond != 0 ||
-        dt.second != 0 ||
-        dt.minute != 0 ||
-        dt.hour != 0;
-  }
-
-  /// 2025년 00월 00일(일) 형태
-  static String formatDateTime(DateTime dt) {
-    final mm = dt.month.toString().padLeft(2, '0');
-    final dd = dt.day.toString().padLeft(2, '0');
-    return '${dt.year}년 $mm월 $dd일(${weekDay2Text(dt.weekday)})';
-  }
-
-  static Future<void> resetApp() async {
-    await Get.deleteAll(force: true);
-    Get.offAllNamed(Routes.SPLASH);
-  }
-  
-  static Uri setDuitUtmSource(Uri uri) {
-    return uri.replace(
-      queryParameters: {
-        ... uri.queryParameters,
-        'utm_source': 'www.dutyit.net'
-      }
-    );
-  }
-
-  static String setDuitUtmSourceString(String uri) {
-    return setDuitUtmSource(Uri.parse(uri)).toString();
   }
 }
